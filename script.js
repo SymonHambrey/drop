eae600483f8a2f1b8a152038f0d0453313f0091b
Symon Hambrey <symonhambrey@gmail.com>
2020-10-29 17:54:23 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const tiles = [
-    [2, '#AA0000'],
+    ['', '#FFF'],
     [4, '#506917'],
     [8, '#7D4E0B'],
     [16, '#1C5F78'],
@@ -24,24 +24,44 @@ let board = [
     [0,0,0,0,0]
 ]
 
+// REMOVE BEFORE ADDING TO APP
+const Click = (elem, func) => {
+    let select = document.querySelectorAll(elem)
+    return select.forEach(item => item.onclick = func )
+  }
+
 const tile_count = tiles.length, max_next = 6
 
 const random = num => Math.floor((Math.random() * num))
 
 let start_tile = tiles[random(max_next)], next = tiles[random(max_next)]
 
+for (let i = 0; i < tiles.length; i ++) {
+    /*
 let next_number = document.getElementById('next_number')
 next_number.innerText = next[0]
 next_number.style.backgroundColor = next[1]
-
+*/
 let drop_tile = document.createElement('DIV')
-drop_tile.style.backgroundColor = start_tile[1]
+drop_tile.style.backgroundColor = tiles[i][1]
 drop_tile.classList.add('number-box')
-drop_tile.innerHTML = `<p>${start_tile[0]}</p>`
+drop_tile.innerHTML = `<p>${tiles[i][0]}</p>`
 drop_tile.id = 'current_drop'
-document.getElementById('ind_col_3').append(drop_tile)
+document.getElementById('tiles').append(drop_tile)
+}
 
+/*
 let current_drop = document.getElementById('current_drop')
 
-    setTimeout(() => { current_drop.style.transform = `translateY(350px)` }, 250)
+Click('.game-column', function() {
+
+    let column_clicked = this.id
+    let tile = document.querySelector('#current_drop').getBoundingClientRect()
+    let tile_icon = document.querySelector('#current_drop')
+console.log(tile.top, tile.right, tile.bottom, tile.left);
+    tile_icon.style.transform = 'translate(-60px, 350px)'
+
+})
+*/
+let canvas = document.getElementById('game_canvas')
 
